0efd4e73c683b1e5a018a1c3869fd4e4c21e2396
Tindur20 <sigsteinn@gmail.com>
2017-04-04 12:19:45 +0000

    byrjaður á login form

--- index.php
+++ index.php
@@ -11,6 +11,23 @@
 	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
 </head>
 <body>
-
+	<div id="center">
+	 	<?php 
+	 	if (isset($_POST['email']) && $_POST['email'] != '' && $_POST['pass'] != '') {
+	 		$email = $_POST['email'];
+	 		$pass = hash('sha256', $_POST['pass']);
+	 		$data = checkUser($conn, $email, $pass);
+	 		echo $data['email'];
+	 		statSession($data['email']);
+	 	  }
+		?>
+		<form method="post" action="index.php">
+			Email:
+			<input type="email" name="email" required>
+			Password:
+			<input type="password" name="pass" required>
+			<input type="submit">
+		</form>
+	</div>
 </body>
 </html>
