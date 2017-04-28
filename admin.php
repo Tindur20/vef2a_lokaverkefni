cf06fd3e7b3b58d1372bfaa694a24fdd665bfe4e
Tindur20 <sigsteinn@gmail.com>
2017-04-28 12:48:49 +0000

    þetta virkar nuna

--- admin.php
+++ admin.php
@@ -1,33 +1,30 @@
 <?php
+// Start the session
 session_unset();
 session_start();
-
 if ($_SESSION['email']) {
-  include 'dbcon.php';
-  include 'dbqueries.php';
-  $email = $_SESSION['email'];
+ include 'dbcon.php';
+ include 'dbqueries.php';
+ $email = $_SESSION['email'];
 }
 else {
-  header('Location: index.php');
+ header('Location: index.php');
 }
-
-    $user = getUsername($conn, $email);
-    echo "Hello " . $user['name'] . "<br>";
-    echo "email : " . $email . "<br>";
-  ?>
-
+ $user = getUserName($conn, $email);
+ echo "Hello " . $user['name'] . "<br>";
+ echo "email : " . $email . "<br>";
+?>
 <a href="logout.php">logout</a>
 
 <?php
-    if (isset($_POST['name']) && $_POST['name']!='') {
-      $n = $_POST['name'];
-      changeUsername($conn, $e, $n);
-      header("Refresh:0");
-    }
-    
+ if (isset($_POST['name']) && $_POST['name']!='') {
+ $name = $_POST['name'];
+ changeUserName($conn, $email, $name);
+ header("Refresh:0");
+ }
  ?>
 <form method="post" action="admin.php">
-  new name:
-  <input type="name" name="name" required><br>
-  <input type="submit">
+ new name:
+ <input type="name" name="name" required><br>
+ <input type="submit">
 </form>
