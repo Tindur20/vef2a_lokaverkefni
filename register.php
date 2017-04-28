9ace57de8eebf21286924e2b6fd96abb9a89e4b2
Tindur20 <sigsteinn@gmail.com>
2017-04-28 13:24:43 +0000

    lagaði hverning útlitið lítur úr á þessari síðu

--- register.php
+++ register.php
@@ -6,8 +6,16 @@
 <html>
 <head>
 	<title>Sing up</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
 </head>
 <body>
+	<div class="navigation">
+		<a href="index.php">Sign up</a>
+	
+<div class="container">
   <?php
       if (isset($_POST['name']) && $_POST['name']!='' && $_POST['email'] !='' && $_POST['pass'] !='') {
         $name = $_POST['name'];
@@ -19,15 +27,18 @@
         header('location: index.php');
       }
    ?>
-  <form method="post" action="register.php">
-    name:
-    <input type="text" name="name" required><br>
-    email:
-    <input type="email" name="email" required><br>
-    password:
-    <input type="password" name="pass" required><br>
-    <input type="submit">
-  </form>
-    <a href="index.php">Login</a>
+  
+   
+    <form method="post" action="index.php">
+	     	name:
+	    	<input type="text" name="name" required>
+			Email:
+			<input type="email" name="email" required>
+			Password:
+			<input type="password" name="pass" required>
+			<input type="submit">
+		</form>
+  </div>
+  </div>
 </body>
 </html>
