b96419018a672debdedbabeeb2d4dfc1b55e63cb
Tindur20 <sigsteinn@gmail.com>
2017-04-28 13:50:16 +0000

    lagaði hverning síðan lítur út

--- index.php
+++ index.php
@@ -6,15 +6,12 @@
 <html>
 <head>
 	<title>Lokaverkefni</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="main.css">
-	<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
+	<?php require 'includes/head.php'; ?>
 </head>
 <body>
 
 	<div class="navigation">
-		<a id="login" href="register.php">Sign up</a>
+		<a href="register.php">Sign up</a>
 	
 	<div class="container">
 	 	<?php 
