244b12779f507411aa89d13368022c46a028af0b
Tindur20 <sigsteinn@gmail.com>
2017-05-02 10:48:39 +0000

    Byrja að gera image uploader

---
+++ imageUpload.php
@@ -0,0 +1,14 @@
+<?php 	
+	include "dbcon.php";		// gagnagrunnstenging
+	include "dbqueries.php";    	// færslur
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lokaverkefni</title>
+	<?php require 'includes/head.php'; ?>
+</head>
+<body>
+
+</body>
+</html>
